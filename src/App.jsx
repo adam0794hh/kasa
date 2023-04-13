ce87d90281c9cc2f5ff0d8bce690751ca1ab3c83
Adam Hammouche <ahammouche@hotmail.com>
2023-04-13 04:03:56 +0200

    feat - Ajout du props main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import "./App.css";
 import Banner from "./components/Banner.jsx";
+import Appartments from "./components/Appartments.jsx";
+import Main from "./components/Main.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Banner/>
+      <Main>
+        <Banner/>
+        <Appartments/>
+      </Main>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Appartments.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Appartments.css"
+import Card from "./Card.jsx"
+
+
+function Appartments() {
+  return (
+    <div className='Appartments'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default Appartments
