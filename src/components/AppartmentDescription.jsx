d324592928b54054a482f99f9089b46f92545484
Adam Hammouche <ahammouche@hotmail.com>
2023-05-15 21:43:31 +0200

    feat - rendre le content vidible et invisible

--- src/components/AppartmentDescription.jsx
+++ src/components/AppartmentDescription.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AppartmentDescription.css"
 
 function AppartmentDescription(props) {
+    const [isContentvisible, setisContentvisible] = useState(false)
+    const showContent = () => { 
+        setisContentvisible(!isContentvisible)
+    }
   return (
-    <div>
+    
         <div className='appartment_description'>
             <p className='description_title'>
                 <span>{props.title}</span>
-                <i class="fa-solid fa-chevron-down fa-lg"></i>
+                <i className ="fa-solid fa-chevron-down fa-lg" onClick={showContent}></i>
             </p>
             
             <div className='description_content'>
-                <p>Vous serez à 50m du canal Saint-martin où vous pourrez
-                    pique-niquer l'été et à côté de nombreux bars et restaurants.Au cœur de Paris avec 5 lignes de métro et de nombreux
-                    bus. Logement parfait pour les voyageurs en solo et les
-                    voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7
-                    minutes à pied).
-                </p>
+                {isContentvisible && <p> {props.content} {props.text}</p>}
             </div>
         </div>
         
-    </div>
+    
   )
 }
 
